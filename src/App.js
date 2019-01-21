bd040a311847ecf0dd16c8c0469eb7f1a02c52e3
elegant-idn <curtisconley34@gmail.com>
2019-01-21 10:30:55 -0500

    added new coming soon home page, removed styles, added icons #5
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './img/papriika-logo-wordmark-white.svg';
+import './css/papriika.css';
 import './css/App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+        <nav class="navbar navbar-expand-lg">
+          <a className="navbar-brand mx-auto mt-3" href="/">
+            <img 
+              src={logo}
+              alt="Papriika logo" 
+              width="175px"
+              />
           </a>
-        </header>
+        </nav>
+        <div className="container">
+          <div className="text-center mt-5">
+            <h1>
+              Coming soon...
+            </h1>
+            <p style={{ fontSize: '18px', marginTop: '16px' }}>The world’s best hotels, hand curated.</p>
+          </div>
+        </div>
       </div>
     );
   }
