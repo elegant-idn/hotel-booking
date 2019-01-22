5f92742fa327329215909a443ccaed958d45a891
elegant-idn <curtisconley34@gmail.com>
2019-01-22 10:06:57 -0500

    built hotel search navbar #7
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/components/HeaderDark.js
+++ src/components/HeaderDark.js
@@ -4,29 +4,30 @@ import logo from './../img/papriika-logo-wordmark-2-white.svg';
 class HeaderDark extends Component {
   render() {
     return (
-      <div className="bg-white fixed-top navbar-spacer">
-        <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
-          <a href="/" className="navbar-brand">
-            <img 
-              src={logo} 
-              className="navbar-brand-img"
-              alt="Papriika"
-            />
-          </a>
-          <div className="collapse navbar-collapse" id="navbarText">
-            <div className="navbar-text text-white mx-4"
-            style={{
-              fontSize: '15px',
-              fontWeight: '700'
-            }}>
-              The world’s best hotels, hand curated
-            </div>
-            <div className="navbar-nav ml-auto">
-              <a className="nav-link" href="#">Contact</a>
-            </div>
+      // to add 15px white space
+      // <div className="bg-white fixed-top navbar-spacer">
+      <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
+        <a href="/" className="navbar-brand">
+          <img 
+            src={logo} 
+            className="navbar-brand-img"
+            alt="Papriika"
+          />
+        </a>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <div className="navbar-text text-white mx-4"
+          style={{
+            fontSize: '15px',
+            fontWeight: '700'
+          }}>
+            The world’s best hotels, hand curated
           </div>
-        </nav>
-      </div>
+          <div className="navbar-nav ml-auto">
+            <a className="nav-link" href="#">Contact</a>
+          </div>
+        </div>
+      </nav>
+      // </div>
     );
   }
 }
--- src/components/HeaderSearch.js
+++ src/components/HeaderSearch.js
@@ -16,7 +16,7 @@ class HeaderSearch extends Component {
         }}>
           <nav className="navbar navbar-light navbar-search" 
           style={{ 
-            marginTop: '105px',
+            marginTop: '90px',
             padding: '0'
           }}>           
             <form className="w-100">
@@ -25,89 +25,252 @@ class HeaderSearch extends Component {
                 borderRight: '1px solid #eee',
                 borderLeft: '1px solid #eee',
               }}>
-                <div className="form-group col-md-4 form-group-navbar-search" 
-                style={{
-                  borderRight: '1px solid #eee'
-                }}>
-                  <label for="navbarSearch"
+                <div className="form-group form-group-navbar-search" 
                   style={{
-                    fontSize: '12px',
-                    fontWeight: '700',
-                    textTransform: 'uppercase',
-                    marginTop: '24px',
-                    marginBottom: '0',
-                    marginLeft: '13px'
-                  }}>Where would you like to go?</label>
+                    borderRight: '1px solid #eee'
+                  }}>
+                  <label for="destination"
+                    style={{
+                      width: '250px',
+                      fontSize: '12px',
+                      fontWeight: '700',
+                      textTransform: 'uppercase',
+                      lineHeight: '14px',
+                      marginTop: '29px',
+                      marginBottom: '0',
+                      marginLeft: '20px'
+                    }}>
+                    Where would you like to go?
+                  </label>
                   <input 
-                  type="search" 
-                  className="form-control" 
-                  style={{
-                    border: 'none'
-                  }}
-                  id="navbarSearch" 
-                  placeholder="Destination or hotel name" />
+                    type="text"
+                    role="textbox"
+                    autocomplete="off"
+                    autofocus="autofocus"
+                    maxlength="60"
+                    tabindex="0"
+                    id="destination"
+                    className="form-control"
+                    style={{
+                      border: 'none',
+                      height: '30px',
+                      color: '#005bf7',
+                      fontSize: '16px',
+                      letterSpacing: '-0.03em',
+                      fontWeight: '500',
+                      marginLeft: '7px',
+                      width: '250px'
+                    }}
+                    placeholder="Destination or hotel name"
+                    aria-invalid="false"
+                    aria-controls="destination-dropdown"
+                    aria-label="Where would you like to go?"
+                    aria-activedescendant="destination-dropdown-item-0"
+                    name="destination"
+                  />
                 </div>
                 <div className="form-group col-md-3 form-group-navbar-search"
-                style={{
-                  borderRight: '1px solid #eee'
-                }}>
-                  <label for="exampleFormControlInput1" className="d-block"
                   style={{
-                    fontSize: '12px',
-                    fontWeight: '700',
-                    textTransform: 'uppercase',
-                    marginTop: '24px',
-                    marginBottom: '0',
-                    marginLeft: '13px'
-                  }}>Dates</label>
+                    borderRight: '1px solid #eee'
+                  }}>
+                  <label for="dates" className="d-block"
+                    style={{
+                      fontSize: '12px',
+                      fontWeight: '700',
+                      textTransform: 'uppercase',
+                      lineHeight: '14px',
+                      marginTop: '29px',
+                      marginBottom: '6px',
+                      marginLeft: '13px'
+                    }}>
+                    Dates
+                  </label>
+                  <input
+                    type="text"
+                    role="textbox"
+                    autocomplete="off"
+                    maxlength="10"
+                    tabindex="0"
+                    id="dates"
+                    className="form-control col-md-6 d-inline"
+                    style={{
+                      border: 'none',
+                      height: '30px',
+                      color: '#005bf7',
+                      fontSize: '16px',
+                      letterSpacing: '-0.03em',
+                      fontWeight: '500'
+                    }}
+                    placeholder="Check in"
+                    aria-invalid="false"
+                    aria-controls="dates-dropdown"
+                    aria-label="Check in date"
+                    aria-activedescendant="dates-dropdown-item-0"
+                    name="dates"
+                  />
                   <input 
-                  type="search" 
-                  className="form-control col-md-6 d-inline" 
-                  style={{
-                    border: 'none'
-                  }}
-                  id="navbarSearch" 
-                  placeholder="Check in" />
-                  <input 
-                  type="search" 
-                  className="form-control col-md-6 d-inline" 
-                  style={{
-                    border: 'none'
-                  }}
-                  id="navbarSearch" 
-                  placeholder="Check in" />
+                    type="text"
+                    role="textbox"
+                    autocomplete="off"
+                    maxlength="10"
+                    tabindex="0"
+                    id="dates"
+                    className="form-control col-md-6 d-inline"
+                    style={{
+                      border: 'none',
+                      height: '30px',
+                      color: '#005bf7',
+                      fontSize: '16px',
+                      letterSpacing: '-0.03em',
+                      fontWeight: '500'
+                    }}
+                    placeholder="Check out"
+                    aria-invalid="false"
+                    aria-controls="dates-dropdown"
+                    aria-label="Check out date"
+                    aria-activedescendant="dates-dropdown-item-0"
+                    name="dates"
+                  />
                 </div>
-                <div className="form-group col-md-3 form-group-navbar-search"
-                style={{
-                  borderRight: '1px solid #eee'
-                }}>
-                  <label for="exampleFormControlInput1"
+                <div className="form-group form-group-navbar-search text-center"
                   style={{
-                    fontSize: '12px',
-                    fontWeight: '700',
-                    textTransform: 'uppercase',
-                    marginTop: '24px',
-                    marginBottom: '0',
-                    marginLeft: '13px'
-                  }}>Dates</label>
+                    width: '99.6px',
+                    paddingLeft: '10px'
+                  }}>
+                  <label for="adults"
+                    style={{
+                      fontSize: '12px',
+                      fontWeight: '700',
+                      textTransform: 'uppercase',
+                      lineHeight: '14px',
+                      marginTop: '29px',
+                      marginBottom: '0'
+                    }}>
+                    Adults
+                  </label>
                   <input 
-                  type="search" 
-                  className="form-control" 
-                  style={{
-                    border: 'none'
-                  }}
-                  id="navbarSearch" 
-                  placeholder="Check in" />
+                    type="number"
+                    role="textbox"
+                    autocomplete="off"
+                    maxlength="60"
+                    tabindex="0"
+                    id="adults"
+                    className="form-control"
+                    style={{
+                      border: 'none',
+                      height: '30px',
+                      color: '#005bf7',
+                      fontSize: '16px',
+                      letterSpacing: '-0.03em',
+                      fontWeight: '500',
+                      textAlign: 'center'
+                    }}
+                    placeholder="2"
+                    aria-invalid="false"
+                    aria-controls="adults-dropdown"
+                    aria-label="Number of adults?"
+                    aria-activedescendant="adults-dropdown-item-0"
+                    name="adults"
+                  />
                 </div>
-                <div className="form-group col-md-1 form-group-navbar-search bg-success"
-                style={{
-                  height: '105px',
-                  padding: '0'
-                }}>
-                <button type="submit" class="btn btn-primary rounded-0"
-                style={{
-                  height: '105px'
-                }}>Search</button>
+                <div className="form-group form-group-navbar-search text-center"
+                  style={{
+                    width: '99.6px'
+                  }}>
+                  <label for="children"
+                    style={{
+                      fontSize: '12px',
+                      fontWeight: '700',
+                      textTransform: 'uppercase',
+                      lineHeight: '14px',
+                      marginTop: '29px',
+                      marginBottom: '0'
+                    }}>
+                    Children
+                  </label>
+                  <input 
+                    type="number"
+                    role="textbox"
+                    autocomplete="off"
+                    maxlength="60"
+                    tabindex="0"
+                    id="children"
+                    className="form-control"
+                    style={{
+                      border: 'none',
+                      height: '30px',
+                      color: '#005bf7',
+                      fontSize: '16px',
+                      letterSpacing: '-0.03em',
+                      fontWeight: '500',
+                      textAlign: 'center'
+                    }}
+                    placeholder="0"
+                    aria-invalid="false"
+                    aria-controls="children-dropdown"
+                    aria-label="Number of children?"
+                    aria-activedescendant="children-dropdown-item-0"
+                    name="children"
+                  />
+                </div>
+                <div className="form-group form-group-navbar-search text-center" 
+                  style={{
+                    width: '99.6px',
+                    paddingRight: '10px'
+                  }}>
+                  <label for="rooms"
+                    style={{
+                      fontSize: '12px',
+                      fontWeight: '700',
+                      textTransform: 'uppercase',
+                      lineHeight: '14px',
+                      marginTop: '29px',
+                      marginBottom: '0'
+                    }}>
+                    Rooms
+                  </label>
+                  <input 
+                    type="number"
+                    role="textbox"
+                    autocomplete="off"
+                    maxlength="60"
+                    tabindex="0"
+                    id="rooms"
+                    className="form-control"
+                    style={{
+                      border: 'none',
+                      height: '30px',
+                      color: '#005bf7',
+                      fontSize: '16px',
+                      letterSpacing: '-0.03em',
+                      fontWeight: '500',
+                      textAlign: 'center'
+                    }}
+                    placeholder="1"
+                    aria-invalid="false"
+                    aria-controls="rooms-dropdown"
+                    aria-label="Number of rooms?"
+                    aria-activedescendant="rooms-dropdown-item-0"
+                    name="rooms"
+                  />
+                </div>
+                <div className="form-group form-group-navbar-search ml-auto"
+                  style={{
+                    height: '105px',
+                    padding: '0'
+                  }}>
+                  <button type="submit" class="btn btn-primary rounded-0"
+                    style={{
+                      fontSize: '12px',
+                      fontWeight: '700',
+                      textTransform: 'uppercase',
+                      lineHeight: '14px',
+                      height: '105px',
+                      paddingRight: '40px',
+                      paddingLeft: '40px'
+                    }}>
+                    Search
+                  </button>
                 </div>
               </div>
             </form>
